1409cd49e9caacd81acbbfb2a4e083ed5ec14050
Neemias Brito <neeemiasaimeeen@gmail.com>
2023-09-17 10:32:19 -0300

    Frist Commit

---
+++ src/main/java/br/com/springboot/Crud_SpringBoot/Application.java
@@ -0,0 +1,15 @@
+package br.com.springboot.Crud_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
